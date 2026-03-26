5421847059a41dabb429c79c936aa1be891b0baf
JoaoPardal-550Desktop <jflpardal@gmail.com>
2026-03-26 11:30:32 +0000

    Added UI element responsible for showing Ammo count. * No functionality yet, just the basic visuals ( `SegmentedBar` and `BarSegments` ). This was added to the HUD and is showing 3 rounded green rectangles * moved created BPs to a folder specific to the project

---
+++ Source/MLlike/BarSegmentWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MLlikeWidget.h"
+#include "BarSegmentWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MLLIKE_API UBarSegmentWidget : public UMLlikeWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MLlike/SegmentedBarWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MLlikeWidget.h"
+#include "SegmentedBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MLLIKE_API USegmentedBarWidget : public UMLlikeWidget
+{
+	GENERATED_BODY()
+	
+};
